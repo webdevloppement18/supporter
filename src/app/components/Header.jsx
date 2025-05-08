89ffa06080ba8bad465e10457596c3d2f78c9418
webdevlopmment18 <webdevlopement18@gmail.com>
2025-05-08 10:25:55 +0200

    nex commit

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,29 @@
+import Link from "next/link"
+
+export default function Header() {
+  return (
+    <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
+          <ul className="flex gap-4">
+              <li>
+                  <Link href={'/sign-in'}>S'enregistrer</Link>
+              </li>
+    
+          <li className="hidden sm:block">
+              <Link href={'/'}>Acceuil</Link>
+          </li>
+      
+       
+            <li className="hidden sm:block">
+                <Link href={'/sign-in'}>a propos du site </Link>
+            </li>
+            </ul>
+
+                <Link href={'/'} className="flex gap-1 items-center">
+                 <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg "> Supporter de foot </span>
+                 <span className="text-xl hidden sm:inline"> de france</span>
+                 </Link>
+          
+         
+    </div>
+  )
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import Header from "./components/Header";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -22,6 +23,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header/>
         {children}
       </body>
     </html>
