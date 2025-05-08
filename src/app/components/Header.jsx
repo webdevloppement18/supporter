17fa9c9a6de99dfec2cede7fcdebd735d1545e43
webdevlopmment18 <webdevlopement18@gmail.com>
2025-05-08 11:46:34 +0200

    integrate CLerck

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,13 +1,22 @@
 import Link from "next/link"
 import DarkModeSwitch from "./DarkModede"
+import { SignedIn,SignedOut,UserButton } from "@clerk/nextjs"
+
 
 export default function Header() {
   return (
     <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
           <ul className="flex gap-4">
+            <SignedIn>
+             <UserButton/>
+             </SignedIn>
+             <SignedOut>
+              <Link href={'sign-up'}>S'enregistrer dans le monde des ultras</Link>
+             </SignedOut>
               <li>
-                  <Link href={'/sign-in'}>S'enregistrer</Link>
+                  <Link href={'/sign-in'}>rentrer dans votre monde </Link>
               </li>
+           
     
           <li className="hidden sm:block">
               <Link href={'/'}>Acceuil</Link>
@@ -15,7 +24,7 @@ export default function Header() {
       
        
             <li className="hidden sm:block">
-                <Link href={'/sign-in'}>a propos du site </Link>
+                <Link href={'/about'}>a propos du site </Link>
             </li>
             </ul>
             <div className="flex items-center gap-4">
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import Header from "./components/Header";
-import  ThemeCom from '@/app/components/ThemeCom';
+import ThemeCom from '@/app/components/ThemeCom';
+import { ClerkProvider } from "@clerk/nextjs";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -20,15 +22,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ThemeCom>
-          <Header />
-          {children}
-        </ThemeCom>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <ThemeCom>
+            <Header />
+            {children}
+          </ThemeCom>
+        </body>
+      </html>
+    </ClerkProvider>
+
   );
 }
